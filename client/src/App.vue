fc27e3ace6f8c971914d1aa042a9d022a1133d80
Max Twardowski <twardowskimax@gmail.com>
2018-09-22 17:10:45 +0200

    Client: created navbar component

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,14 +1,23 @@
 <template>
   <div id="app">
+    <Navbar />
     <router-view/>
   </div>
 </template>
 
 <script>
+import Navbar from './components/Navbar'
+
 export default {
-  name: 'App'
+  name: 'App',
+  components: {
+    Navbar
+  }
 }
 </script>
 
 <style>
+* {
+  font-family: "Segoe UI", Tahoma, Geneva, Verdana, sans-serif;
+}
 </style>
